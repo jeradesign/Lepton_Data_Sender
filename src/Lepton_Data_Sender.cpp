4dc0b5f266331c77764c3d8e92a990b34413e888
John Brewer <jbrewer@jera.com>
2015-09-15 16:06:20 -0700

    Non-working i2c code.

--- src/Lepton_Data_Sender.cpp
+++ src/Lepton_Data_Sender.cpp
@@ -30,6 +30,7 @@ int socket_fd;
 struct sockaddr_in servaddr;
 
 Spi *spi;
+I2c* i2c;
 Gpio *chipSelect;
 
 uint8_t paranoia1[LINE_SIZE];
@@ -64,6 +65,77 @@ void setupSPI()
 	usleep(200000);
 	chipSelect->write(0);	// low to select chip
 	fprintf(stderr, "select chip\n");
+}
+
+void setupI2C()
+{
+  i2c = new mraa::I2c(1);
+  i2c->address(0x2A);
+  uint16_t result;
+  do {
+    result = i2c->readWordReg(2);
+    fprintf(stderr, "1 i2c result = %d\n", result);
+  } while ((result & 0x0600) != 0x0600);
+
+  do {
+    result = i2c->readWordReg(2);
+    fprintf(stderr, "Wait for camera i2c result = %d\n", result);
+  } while (result & 0x0100); // byte swapped
+
+  // Ping camera
+
+  i2c->writeWordReg(6, 0x0000); // data length (byte swapped!)
+  i2c->writeWordReg(4, 0x0202); // Ping Camera (byte swapped!)
+
+  do {
+    result = i2c->readWordReg(2);
+    fprintf(stderr, "Ping Camera i2c result = %d\n", result);
+  } while (result & 0x0100); // byte swapped
+
+  // Run FFC Normalization
+
+  i2c->writeWordReg(6, 0x0000); // data length (byte swapped!)
+  i2c->writeWordReg(4, 0x4202); // Run FFC Normalization (byte swapped!)
+
+  do {
+    result = i2c->readWordReg(2);
+    fprintf(stderr, "Run FFC Normalization i2c result = %d\n", result);
+  } while (result & 0x0100); // byte swapped
+
+  // Get Serial Number
+
+  i2c->writeWordReg(6, 0x0400); // data length (byte swapped!)
+  i2c->writeWordReg(4, 0x0202); // Enable Telemetry (byte swapped!)
+
+  do {
+    result = i2c->readWordReg(2);
+    fprintf(stderr, "Get Serial Number i2c result = %d\n", result);
+  } while (result & 0x0100); // byte swapped
+
+  fprintf(stderr, "Serial Number 1 = %04x\n", i2c->readWordReg(8));
+  fprintf(stderr, "Serial Number 2 = %04x\n", i2c->readWordReg(10));
+  fprintf(stderr, "Serial Number 3 = %04x\n", i2c->readWordReg(12));
+  fprintf(stderr, "Serial Number 4 = %04x\n", i2c->readWordReg(14));
+
+  // Set Enable Telemetry
+
+  i2c->writeWordReg(6, 0x0200); // data length (byte swapped!)
+  i2c->writeWordReg(8, 0x0100); // 1 LSW (byte swapped!)
+  i2c->writeWordReg(10, 0x0000); // 1 MSW (byte swapped!)
+  i2c->writeWordReg(4, 0x1902); // Enable Telemetry (byte swapped!)
+
+  do {
+    result = i2c->readWordReg(2);
+    fprintf(stderr, "Enable Telemetry i2c result = %d\n", result);
+  } while (result & 0x0100); // byte swapped
+
+  // i2c->writeWordReg(6, 0x0002); // data length
+  // i2c->writeWordReg(8, 0x0000); // 0 LSW
+  // i2c->writeWordReg(10, 0x0000); // 0 MSW
+  // i2c->writeWordReg(4, 0x021d); // Telemetry in header
+  // while (i2c->readWordReg(2) & 0x0001) {
+  //   ;
+  // }
 }
 
 void setupConnection(char *hostname)
@@ -112,6 +184,7 @@ int main(int argc, char **argv)
 	setupConnection(argv[1]);
 
 	setupSPI();
+	setupI2C();
 
 #if LOG_SPI_DATA
 	FILE *fp = fopen("spidata.dat", "wb");
@@ -168,9 +241,9 @@ int main(int argc, char **argv)
 		assert(paranoia5[0] == 0xa5);
 		assert(paranoia5[1] == 0x5a);
 
-		if (!checkFrame()) {
-			continue;
-		}
+		// if (!checkFrame()) {
+		// 	continue;
+		// }
 
 		write(socket_fd, recvFrame, FRAME_SIZE);
 	}
